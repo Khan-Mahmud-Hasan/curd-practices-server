4c30d5bf71cb60a0da1539ff060091f5e49c6c17
Mahmud Hasan <mahmud.hasan2006@gmail.com>
2023-06-01 15:04:12 +0600

    get data from UI client & create database to mongoDb

--- index.js
+++ index.js
@@ -25,6 +25,20 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
+
+        // create database 
+        // const db = client.db(dbName); format
+        const coffeeCollection = client.db('myCoffeeDB').collection('myCoffee')
+
+        // get data from UI-client
+        app.post('/coffees', async (req, res) => {
+            const newCoffee = req.body;
+            console.log(newCoffee);
+            const result = await coffeeCollection.insertOne(newCoffee);
+            res.send(result);
+        })
+
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
